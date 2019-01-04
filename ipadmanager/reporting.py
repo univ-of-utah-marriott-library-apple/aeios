b36f660ec7466277c5095560d0a8270ac878ff02
Sam Forester <swforester@gmail.com>
2019-01-04 10:44:41 -0700

    reporting: 1.0.0
    added reporting module, removed dependencies on management_tools.slack

---
+++ ipadmanager/reporting.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+import json
+import urllib2
+
+import logging
+
+
+'''Library for reporting events with device manager
+'''
+
+__author__ = "Sam Forester"
+__email__ = "sam.forester@utah.edu"
+__copyright__ = "Copyright(c) 2018 University of Utah, Marriott Library"
+__license__ = "MIT"
+__version__ = '1.0.0'
+__url__ = None
+__description__ = ('Library for reporting events with devicemanager')
+
+__all__ = [
+    'SlackSender',
+    'Reporter',
+    'Slack',
+    'reporterFromSettings'
+]
+
+class Error(Exception):
+    pass
+
+
+class Reporter(object):
+    '''Base Reporter class
+    '''
+        
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def send(self, msg):
+        pass
+
+
+class SlackSender(Reporter):
+    '''Minimal functionality of management_tools.slack
+    '''
+    def __init__(self, url, channel, name):
+        self.url = url
+        self.name = name
+        self.channel = channel
+
+    def send(self, msg):
+        json_str = json.dumps({'text': str(msg), 
+                               'username': self.name, 
+                               'channel': self.channel})
+        try:
+            request = urllib2.Request(self.url, json_str)
+            urllib2.urlopen(request)
+        except Exception as e:
+            logging.error("{0}: unable to send message".format(e))
+
+
+class Slack(Reporter):
+    '''
+    '''
+    def __init__(self, url, channel, name='ipadmanager'):
+        self.url = url
+        self.channel = channel
+        self.name = name
+        self.bot = SlackSender(self.url, self.channel, self.name)
+        logging.debug("slack channel: {0}".format(self.channel))
+        logging.debug("slack name: {0}".format(self.name))
+    
+    def send(self, msg):
+        try:
+            self.bot.send(msg)
+        except Exception as e:
+            logging.error(e)
+            logging.error("unable to send message: {0}".format(msg))
+
+
+def reporterFromSettings(conf):
+    '''Returns appropriate reporter object based upon settings
+    '''
+    try:
+        _slack = conf['Slack']
+        name = _slack.get('name')
+        return Slack(_slack['url'], _slack['channel'], name)
+    except KeyError:
+        class NullReporter(Reporter)
+            pass
+        return NullReporter()
+
