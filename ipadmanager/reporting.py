d05eea1426117385fc7f3156f990652b86dac850
Sam Forester <swforester@gmail.com>
2019-01-09 16:17:15 -0700

    reporting: 1.0.4
    added logging, changed default logging, modified exception handling

--- ipadmanager/reporting.py
+++ ipadmanager/reporting.py
@@ -2,20 +2,19 @@
 
 import json
 import urllib2
-
 import logging
-
 
 '''Library for reporting events with device manager
 '''
 
 __author__ = "Sam Forester"
 __email__ = "sam.forester@utah.edu"
-__copyright__ = "Copyright(c) 2018 University of Utah, Marriott Library"
+__copyright__ = ("Copyright(c) 2018 "
+                 "University of Utah, Marriott Library")
 __license__ = "MIT"
-__version__ = '1.0.2'
+__version__ = '1.0.4'
 __url__ = None
-__description__ = ('Library for reporting events with devicemanager')
+__description__ = 'Library for reporting events with devicemanager'
 
 __all__ = [
     'SlackSender',
@@ -23,6 +22,10 @@ __all__ = [
     'Slack',
     'reporterFromSettings'
 ]
+
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s')
+LOGGER = logging.getLogger(__name__)
+
 
 class Error(Exception):
     pass
@@ -39,11 +42,16 @@ class Reporter(object):
         pass
 
 
+class NullReporter(Reporter):
+    '''Does nothing
+    '''
+    pass
+
+
 class SlackSender(Reporter):
     '''Minimal functionality of management_tools.slack
     '''
     def __init__(self, url, channel, name, log=None):
-        self.log = log if log else logging
         self.url = url
         self.name = name
         self.channel = channel
@@ -52,24 +60,20 @@ class SlackSender(Reporter):
         json_str = json.dumps({'text': str(msg), 
                                'username': self.name, 
                                'channel': self.channel})
-        try:
-            request = urllib2.Request(self.url, json_str)
-            urllib2.urlopen(request)
-        except Exception as e:
-            self.log.error("{0}: unable to send message".format(e))
-
+        request = urllib2.Request(self.url, json_str)
+        urllib2.urlopen(request)
 
 class Slack(Reporter):
+    '''Class for reporting using Slack
     '''
-    '''
-    def __init__(self, url, channel, name='ipadmanager', log=None):
-        self.log = log if log else logging
+    def __init__(self, url, channel, name='ipadmanager', log=LOGGER):
+        self.log = log
         self.url = url
         self.channel = channel
         self.name = name
-        self.bot = SlackSender(self.url, self.channel, self.name)
-        self.log.debug("slack channel: {0}".format(self.channel))
-        self.log.debug("slack name: {0}".format(self.name))
+        self.bot = SlackSender(url, channel, name, log=log)
+        self.log.debug("Slack: channel: {0}".format(self.channel))
+        self.log.debug("Slack: name: {0}".format(self.name))
     
     def send(self, msg):
         try:
@@ -78,15 +82,17 @@ class Slack(Reporter):
             self.log.error(e)
 
 
-def reporterFromSettings(conf, log=None):
+def reporterFromSettings(conf, log=LOGGER):
     '''Returns appropriate reporter object based upon settings
     '''
     try:
         _slack = conf['Slack']
         name = _slack.get('name')
         return Slack(_slack['url'], _slack['channel'], name, log)
-    except KeyError:
-        class NullReporter(Reporter):
-            pass
+    except KeyError as e:
+        log.error("missing key: {0}".format(e))
+        log.debug("returning NullReporter()".format(e))
         return NullReporter()
 
+if __name__ == '__main__':
+    pass
