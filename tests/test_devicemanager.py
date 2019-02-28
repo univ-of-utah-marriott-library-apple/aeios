570cf45b500e484d109318fe8835aad17f0c5e30
Sam Forester <swforester@gmail.com>
2019-02-28 14:30:25 -0700

    added tests for devicemanager

--- tests/test_devicemanager.py
+++ tests/test_devicemanager.py
@@ -4,11 +4,16 @@
 import os
 import shutil
 import unittest 
+import logging
+import types
+from datetime import datetime, timedelta
+
+from actools import cfgutil
 
 '''Tests for ipadmanager.devicemanager
 '''
 
-from devicemanager import DeviceManager
+import aeios
 
 __author__ = "Sam Forester"
 __email__ = "sam.forester@utah.edu"
@@ -34,21 +39,24 @@ def setUpModule():
 def tearDownModule():
     '''remove tmp directory
     '''
-    shutil.rmtree(TMPDIR)
+    #shutil.rmtree(TMPDIR)
+    pass
 
 
 class BaseTestCase(unittest.TestCase):
     
     file = None
+    tmp = TMPDIR
+    logger = None
 
     @classmethod
     def setUpClass(cls):
-        cls.tmp = TMPDIR
+        cls.logger = logging.getLogger(__name__)
         cls.env = [{'ECID': '0x123456789ABCD0',
                     'UDID': 'a0111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-1',
+                    'deviceName': 'test-ipad-1',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000001'},
@@ -56,7 +64,7 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a1111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-2',
+                    'deviceName': 'test-ipad-2',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000002'},
@@ -64,7 +72,7 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a2111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-3',
+                    'deviceName': 'test-ipad-3',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000003'},
@@ -72,7 +80,7 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a3111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-4',
+                    'deviceName': 'test-ipad-4',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000004'},
@@ -80,7 +88,7 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a4111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-5',
+                    'deviceName': 'test-ipad-5',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000005'},
@@ -88,7 +96,7 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a5111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-6',
+                    'deviceName': 'test-ipad-6',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000006'},
@@ -96,7 +104,7 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a6111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-7',
+                    'deviceName': 'test-ipad-7',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000007'},
@@ -104,7 +112,7 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a7111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-8',
+                    'deviceName': 'test-ipad-8',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000008'},
@@ -112,7 +120,7 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a8111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-9',
+                    'deviceName': 'test-ipad-9',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000009'},
@@ -120,28 +128,262 @@ class BaseTestCase(unittest.TestCase):
                     'UDID': 'a9111222333444555666777888999abcdefabcde',
                     'bootedState': 'Booted',
                     'buildVersion': '15G77',
-                    'deviceName': 'checkout-ipad-10',
+                    'deviceName': 'test-ipad-10',
                     'deviceType': 'iPad7,5',
                     'firmwareVersion': '11.4.1',
                     'locationID': '0x00000010'}]
         
     @classmethod
     def tearDownClass(cls):
-        os.remove(cls.file)
+        # os.remove(cls.file)
+        pass
         
     def setUp(self):
         id = 'edu.utah.mlib.test'
-        self.path = self.__class__.tmp 
-        self.manager = DeviceManager(id, path=self.path)
+        self.path = self.__class__.tmp
+        logging.basicConfig(level=logging.CRITICAL)
+        self.manager = aeios.DeviceManager(id, path=self.path)
         self.env = self.__class__.env
+        self.info = self.env
+        self.now = datetime.now()
+        self.devices = []
         self.__class__.file = self.manager.file
     
     def tearDown(self):
-        pass
+        self.logger.setLevel(logging.CRITICAL)
 
+
+class TestCheckin(BaseTestCase):
+
+    def test_checkin(self):
+        for info in self.info:
+            self.manager.checkin(info, run=False)
+
+
+class TestCheckout(BaseTestCase):
+
+    def test_never_checked_in(self):
+        '''Test that a device that has never been checked in
+        '''
+        for info in self.info:
+            self.manager.checkout(info)
+
+
+class TestNeedsErase(BaseTestCase):
+    '''Tests for detecting if a device needs to be erased
+    '''
+    
+    def setUp(self):
+        super(self.__class__, self).setUp()
+        d = self.env[0]
+        self.device = self.manager.device(d['ECID'], d)
+        self.now = datetime.now()
+    
+    def test_unmanaged_device(self):
+        '''test unmanaged devices will not be erased
+        '''
+        ## Object Method patching
+        #  replace our manager's managed function with one that simply 
+        #  returns False. The function will be reset on next setUp
+        def _dummy(self, x):
+            return False
+        self.manager.managed = types.MethodType(_dummy, self.manager)
+
+        self.assertFalse(self.manager.need_to_erase(self.device))
+
+    def test_not_checkedin(self):
+        '''test non-checkedin device will be erased
+        '''
+        self.device.checkin = None
+        self.assertTrue(self.manager.need_to_erase(self.device))
+        self.device.checkin = datetime.now()
+
+    def test_restarting(self):
+        '''test restarting device will be erased
+        '''
+        self.device.restarting = True
+        self.assertFalse(self.manager.need_to_erase(self.device))
+        
+    def test_quick_disconnect(self):
+        '''test devices that quickly disconnect and reconnect
+        '''
+        self.device.erased = self.now - timedelta(minutes=5)
+        self.device.checking = self.now - timedelta(seconds=10)
+        self.device.checkout = self.now
+        self.device.restarting = False
+        # self.logger.setLevel(logging.DEBUG)
+        self.assertFalse(self.manager.need_to_erase(self.device))
+
+    def test_not_erased(self):
+        '''test devices that have not been erased will be
+        '''
+        self.device.erased = None
+        # self.logger.setLevel(logging.DEBUG)
+        self.assertTrue(self.manager.need_to_erase(self.device))
+
+    def test_erased_more_than_timeout_with_blink(self):
+        '''test device was erased but exceed the timeout
+        '''
+        self.device.erased = self.now - timedelta(minutes=12)
+        self.device.checkin = self.now - timedelta(seconds=10)
+        self.device.checkout = self.now
+        self.restarting = False
+        # self.logger.setLevel(logging.DEBUG)
+        self.assertFalse(self.manager.need_to_erase(self.device))
+
+    def test_valid_checkout(self):
+        '''test valid device checkout
+        '''
+        device = self.manager.device(self.device.ecid)
+        self.device.erased = self.now - timedelta(hours=3)
+        self.device.checkin = self.now - timedelta(hours=2)
+        self.device.checkout = self.now - timedelta(hours=1)
+        self.device.restarting = False
+        # self.logger.setLevel(logging.DEBUG)
+        self.assertTrue(self.manager.need_to_erase(self.device))
+
+
+class TestListRefresh(BaseTestCase):
+    '''Complicated tests relying on the replacement of an underlying
+    function used by aeios.DeviceManager.list()
+
+    tests verify that the manager caches the results to a file and
+    reads the cached results when appropriate as well as refreshes
+    the cache when appropriate
+    '''
+    @classmethod
+    def setUpClass(cls):
+        super(cls, cls).setUpClass()
+        # save original cfgutil.list function for restore after tests
+        cls._cfglist = cfgutil.list
+    
+    @classmethod
+    def tearDownClass(cls):
+        # restore original cfgutil.list function
+        cfgutil.list = cls._cfglist
+    
+    def setUp(self):
+        '''
+        '''
+        # super(self.__class__, self).setUp()
+        BaseTestCase.setUp(self)
+        # replacement function to return simple list
+        self.listed = []
+        for d in self.env[0:2]:
+            m = {k:d[k] for k in ['UDID', 'ECID', 'locationID', 
+                                  'deviceName', 'deviceType']}
+            self.listed.append(m)
+        # manually modify the config file with the cached list
+        self.manager.config.update({'cfgutilList':self.listed,
+                                    'lastListed':self.now})
+
+        # replacement function to return empty list
+        def _empty(*args, **kwargs):
+            return []
+        self.empty = _empty
+
+        # replacement function to return simple device list
+        def _simple(*args, **kwargs):
+            return self.listed
+        self.simple = _simple
+        
+        # replace cfgutil.list with _simple() above
+        cfgutil.list = _simple
+        self.listed = self.manager.list()
+        # verify function replacement worked
+        self.assertEquals(self.listed, _simple())
+ 
+    def tearDown(self):
+        # super(self.__class__, self).tearDown()
+        BaseTestCase.tearDown(self)
+
+        # remove any cached values
+        try:
+            self.manager.config.delete('lastListed')
+        except:
+            pass
+        try:
+            self.manager.config.delete('cfgutilList')
+        except:
+            pass
+
+    def test_default_list(self):
+        '''test that given default empty values, cached values are 
+        populated
+        '''
+        # delete the values configured in setUp()
+        self.manager.config.delete('lastListed')
+        self.manager.config.delete('cfgutilList')
+
+        # re-replace cfgutil.list and re-run
+        cfgutil.list = self.empty
+        self.manager.list()
+
+        # Actual tests
+        timestamp = self.manager.config.get('lastListed')
+        cached = self.manager.config.get('cfgutilList')
+        self.assertIsNotNone(timestamp)
+        self.assertIsNotNone(cached)
+        
+    def test_cached_list_returned(self):
+        '''test cached result is returned
+        '''
+        # re-replace cfgutil.list (should not be called)
+        cfgutil.list = self.empty
+        listed = self.manager.list()
+        # re-run manager.list(), should return cached results
+        # (not the replaced function)
+        self.assertEquals(listed, self.listed)
+
+    def test_results_cached_to_file(self):
+        '''test cached result is written to file
+        '''
+        listed = self.manager.list()
+        file_cache = self.manager.config.get('cfgutilList')
+        self.assertItemsEqual(listed, file_cache)
+
+    def test_cached_results_expire(self):
+        '''test cached result expires 
+        '''
+        # manually modify the lastListed to 1 minute ago (force update)
+        timestamp = self.now - timedelta(minutes=1)
+        self.manager.config.update({'lastListed':timestamp})
+
+        # re-replace cfgutil.list
+        cfgutil.list = self.empty
+        # verify manager.list() returns results from second replacement
+        result = self.manager.list()
+        self.assertEquals(result, self.empty())
+
+    def test_forced_refresh(self):
+        '''test list can be forcibly refreshed
+        '''
+        # re-replace cfgutil.list
+        cfgutil.list = self.empty
+        # verify manager.list() returns results from second replacement
+        result = self.manager.list(refresh=True)
+        # verify manager.list() refreshes the list
+        self.assertEquals(result, self.empty())
+
+
+class TestCache(BaseTestCase):
+    
+    def setUp(self):
+        BaseTestCase.setUp(self)
+        self.cache = aeios.Cache(self.manager.config)
+
+
+class TestVerify(BaseTestCase):
+    
+#     def setUp(self):
+#         super(self.__class__, self).setUp()
+    
+    def test_devices(self):
+        pass
 
 class TestThreaded(BaseTestCase):
     pass
+
 
 
 if __name__ == '__main__':
